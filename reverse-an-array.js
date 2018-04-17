31b16402941b66b4931f38b25e47d4a4ab4ace5e
sarasaurus <bixler.s.e@gmail.com>
2018-04-17 13:43:28 -0700

    added code, inited folder, linter working

--- reverse-an-array.js
+++ reverse-an-array.js
@@ -1,8 +1,8 @@
-let arr = [1, 2, 3, 4, 5, 6, 7];
 function reverseArray(arr) {
-  let newArr=[];
-  for (i=0; i<=arr.length; i++) {
+  const newArr = [];
+  for (let i = 0; i <= arr.length; i++) {
     newArr.push(arr[arr.length - i]);
   }
   return newArr;
 }
+reverseArray([1, 2, 3, 4, 5, 6]);
