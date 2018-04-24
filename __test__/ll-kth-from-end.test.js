8e3854eb311c222a532c9a569cfc562a77c30c92
sarasaurus <bixler.s.e@gmail.com>
2018-04-24 14:37:45 -0700

    all scaffolded about to test

---
+++ __test__/ll-kth-from-end.test.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const LinkedList = require('../ll_kth_from_end');
+
+describe('ll_kth_from_end.js', () => {
+  test('#constructor', () => {
+    const testList = new LinkedList();
+    expect(testList.head).toBeNull();
+  });
+  test('#k = 0', () => {
+    const testList = new LinkedList();
+    testList.append(1);
+    testList.append(3);
+    testList.append(8);
+    testList.append(2);
+    expect(testList.kthFromEnd(0)).toEqual(testList.head.next.next.next);
+  });
+  test('#k = 2', () => {
+    const testList = new LinkedList();
+    testList.append(1);
+    testList.append(3);
+    testList.append(8);
+    testList.append(2);
+    expect(testList.kthFromEnd(2)).toEqual(testList.head.next);
+  });
+  test('#k = 6', () => {
+    const testList = new LinkedList();
+    testList.append(1);
+    testList.append(3);
+    testList.append(8);
+    testList.append(2);
+    expect(testList.kthFromEnd(6)).toEqual('exception');
+  });
+});
