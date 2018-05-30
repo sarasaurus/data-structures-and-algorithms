10d6a6cf7ba67e0b2a2d55bd9be4c11c96371fdc
sarasaurus <bixler.s.e@gmail.com>
2018-05-30 16:23:58 -0700

    added second test, but is not working

--- __test__/tree-intersection.test.js
+++ __test__/tree-intersection.test.js
@@ -4,7 +4,7 @@ import BinaryTree from '../lib/binary-tree';
 import Node from '../lib/tree-node';
 import findIntersection from '../tree-intersection';
 
-describe('testing tree-intersection', () => {
+describe('testing tree-intersection simple', () => {
   const one = new Node(1);
   const two = new Node(2);
   const three = new Node(3);
@@ -39,3 +39,39 @@ describe('testing tree-intersection', () => {
     expect(findIntersection(treeA, treeB)).toEqual([1, 2, 3]);
   });
 });
+// describe('testing tree-intersection, handle duplicates', () => {
+//   let treeA = null;
+//   let treeB = null;
+//   const one = new Node(1);
+//   const two = new Node(2);
+//   const three = new Node(3);
+//   const four = new Node(4);
+//   const five = new Node(5);
+//   const six = new Node(6);
+
+//   const a = new Node(10);
+//   const b = new Node(2);
+//   const c = new Node(3);
+//   const d = new Node(1);
+//   const e = new Node(50);
+//   const f = new Node(60);
+
+//   one.left = two;
+//   one.right = three;
+//   three.left = three;
+//   three.right = three;
+//   two.left = six;
+//   two.right = six;
+
+//   a.left = b;
+//   a.right = c;
+//   b.left = b;
+//   b.right = f;
+
+//   treeA = new BinaryTree(one);
+//   treeB = new BinaryTree(a);
+
+//   test('it should return an array of [1, 2, 3]', () => {
+//     expect(findIntersection(treeA, treeB)).toEqual([1, 2, 3]);
+//   });
+// });
--- tree-intersection.js
+++ tree-intersection.js
@@ -1,7 +1,5 @@
 'use strict';
 
-import BinaryTree from './lib/binary-tree';
-
 const findIntersection = (a, b) => {
   const map = new Map();
   const output = [];
@@ -31,19 +29,3 @@ const findIntersection = (a, b) => {
   return output;
 };
 export default findIntersection;
-
-
-// / build hashmap based on treeA
-// push all values in a, as keys with value true
-// map.set(nodeVal: true);
-// then traverse treeB and check if each nodeval is in the map
-// if it is push to map/array/storage
-// return the collection
-// Hshmaps are greate for looking, counting, finding dups and keeping track of values THE ONLY time hashmaps not so great are to fine the 'closest value'
-
-// be cautious with number: number-- becasue if you have 0: 0--> it would evaluate to false becasue 0 is false
-
-// ES 6 set is like a map but values must be unique
-
-// object in js IS a hashmap basically, just more limited
-// hasmap is like a JS object, but where keys can be anything
