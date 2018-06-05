a6370aff85e280c026b5d2730a5973217dd128ec
sarasaurus <bixler.s.e@gmail.com>
2018-06-04 23:47:49 -0700

    linter fixes

--- __test__/mergesort.test.js
+++ __test__/mergesort.test.js
@@ -1,3 +1,5 @@
+'use strict';
+
 import mergesort from '../mergesort';
 
 describe('#testing mergesort', () => {
@@ -5,7 +7,7 @@ describe('#testing mergesort', () => {
     expect(mergesort([5, 9, 10, 3, 2, 7, 8, 1, 6, 4])).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   });
   test('it should return -3,-2,1,2,3,4,5,6,7', () => {
-    expect(mergesort([5, -1, -3, 3, 2, 7, -2, 1, 6, 4])).toEqual([-3, -2, 1, 2, 3, 4, 5, 6, 7]);
+    expect(mergesort([5, -1, -3, 3, 2, 7, -2, 1, 6, 4])).toEqual([-3, -2, -1, 1, 2, 3, 4, 5, 6, 7]);
   });
   test('it should return error for empty array', () => {
     expect(mergesort([])).toEqual('invalid input');
--- mergesort.js
+++ mergesort.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const mergesort = (arr) => {// ?
+const mergesort = (arr) => {
   if (!arr.length) {
     return 'invalid input';
   } else if (arr.length < 2) {
     return arr;
   }
-  const len = arr.length; // ?
-  const half = Math.floor(len / 2); // ?
-  const left = arr.slice(0, half); // ?
+  const len = arr.length; 
+  const half = Math.floor(len / 2); 
+  const left = arr.slice(0, half); 
   const right = arr.slice(half, len); 
 
   const merge = (leftSort, rightSort) => {
@@ -26,18 +26,16 @@ const mergesort = (arr) => {// ?
         result.push(rightSort.shift());// ?
       }
     }
-    while(leftSort.length) {
+    while (leftSort.length) {
       result.push(leftSort.shift());
     }
-    while(rightSort.length) {
+    while (rightSort.length) {
       result.push(rightSort.shift());
     }
     return result; // ?
   };
   return merge(mergesort(left), mergesort(right));// ?
 };
-const test = [4, 7, 2, 1, 10, 78, 3, 45, 0, 50];
-const poop = mergesort('poop'); // ?
-console.log(poop);
+
 export default mergesort;
 
